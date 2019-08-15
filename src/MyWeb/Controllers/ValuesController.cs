7ac81c0c4f9598589def04112e1dee81473eb6f8
wk-j <somnuk.wk@gmail.com>
2019-08-15 18:19:12 +0700

    Update ValuesController.cs

--- src/MyWeb/Controllers/ValuesController.cs
+++ src/MyWeb/Controllers/ValuesController.cs
@@ -19,10 +19,7 @@ namespace MyWeb.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+        public ActionResult<string> Get(int id) => "value";
 
         // POST api/values
         [HttpPost]
